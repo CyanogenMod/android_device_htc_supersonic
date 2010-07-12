91b0a75d1680a8870027e4bb5072efd3b05136cb
Steve Kondik <shade@chemlab.org>
2010-07-12 11:09:04 -0400

    Slim down the list of proprietaries.
    Some of these will likely be readded in the future. Baby steps :)

--- extract-files.sh
+++ extract-files.sh
@@ -21,20 +21,11 @@ mkdir -p ../../../vendor/htc/$DEVICE/proprietary
 # This needs to be removed once .32 kernel is ported.
 adb pull /system/lib/hw/gralloc.qsd8k.so ../../../vendor/htc/$DEVICE/proprietary
 
-adb pull /system/bin/PPST ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/akmd ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/aparse ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/awb_camera ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/bma150_usr ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/bootcomplete ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/btld ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/cam_ins_spmo ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/debug_tool ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/dmagent ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/fbtool ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/htclogkernel ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/ser2net ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/bin/setmic ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/snd ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/snd8k ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/bin/sound8k ../../../vendor/htc/$DEVICE/proprietary
@@ -45,9 +36,6 @@ adb pull /system/etc/AudioBTID.csv ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/TPA2018.csv ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/WPDB.zip ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/agps_rm ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/etc/voVidDec.dat ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/etc/vo_omx_core.cfg ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/etc/vomeOne.cfg ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/firmware/Supersonic_20100204_Sprint_Jack_324016_v2.acdb ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/firmware/BCM4329B1_002.002.023.0360.0361.hcd ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/etc/firmware/default.acdb ../../../vendor/htc/$DEVICE/proprietary
@@ -58,10 +46,8 @@ adb pull /system/etc/pvasflocal.cfg ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/egl/libEGL_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/egl/libGLESv1_CM_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/egl/libGLESv2_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/libOlaBase.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/libOmxAacEnc.so ../../../vendor/htc/$DEVICE/proprietary
+adb pull /system/lib/egl/libq3dtools_adreno200.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libOmxCore.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/libOmxQcelp13Enc.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libOmxVdec.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libOmxVidEnc.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libcamera.so ../../../vendor/htc/$DEVICE/proprietary
@@ -71,20 +57,12 @@ adb pull /system/lib/libhtc_acoustic.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libhtc_ril.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libmm-omxcore.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/liboemcamera.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/libomx_vo_omx_core.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libomx_wmadec_sharedlibrary.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libomx_wmvdec_sharedlibrary.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libpvasfcommon.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libpvasflocalpb.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libpvasflocalpbreg.so ../../../vendor/htc/$DEVICE/proprietary
 adb pull /system/lib/libspeech.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/voEVRCDec.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/voEVRCEnc.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/voOMXOne.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/voQCELPDec.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/voQCELPEnc.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/voVidDec.so ../../../vendor/htc/$DEVICE/proprietary
-adb pull /system/lib/libon2.so ../../../vendor/htc/$DEVICE/proprietary
 
 (cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/htc/$DEVICE/$DEVICE-vendor-blobs.mk
 # Copyright (C) 2010 The Android Open Source Project
@@ -132,16 +110,11 @@ PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \\
     vendor/htc/__DEVICE__/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \\
     vendor/htc/__DEVICE__/proprietary/pvasflocal.cfg:/system/etc/pvasflocal.cfg \\
-    vendor/htc/__DEVICE__/proprietary/voVidDec.dat:/system/etc/voVidDec.dat \\
-    vendor/htc/__DEVICE__/proprietary/vo_omx_core.cfg:/system/etc/vo_omx_core.cfg \\
-    vendor/htc/__DEVICE__/proprietary/vomeOne.cfg:/system/etc/vomeOne.cfg \\
     vendor/htc/__DEVICE__/proprietary/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \\
     vendor/htc/__DEVICE__/proprietary/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \\
     vendor/htc/__DEVICE__/proprietary/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \\
-    vendor/htc/__DEVICE__/proprietary/libOlaBase.so:/system/lib/libOlaBase.so \\
-    vendor/htc/__DEVICE__/proprietary/libOmxAacEnc.so:/system/lib/libOmxAacEnc.so \\
+    vendor/htc/__DEVICE__/proprietary/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \\
     vendor/htc/__DEVICE__/proprietary/libOmxCore.so:/system/lib/libOmxCore.so \\
-    vendor/htc/__DEVICE__/proprietary/libOmxQcelp13Enc.so:/system/lib/libOmxQcelp13Enc.so \\
     vendor/htc/__DEVICE__/proprietary/libOmxVdec.so:/system/lib/libOmxVdec.so \\
     vendor/htc/__DEVICE__/proprietary/libOmxVidEnc.so:/system/lib/libOmxVidEnc.so \\
     vendor/htc/__DEVICE__/proprietary/libcamera.so:/system/lib/libcamera.so \\
@@ -151,22 +124,12 @@ PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \\
     vendor/htc/__DEVICE__/proprietary/libmm-omxcore.so:/system/lib/libmm-omxcore.so \\
     vendor/htc/__DEVICE__/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \\
-    vendor/htc/__DEVICE__/proprietary/libomx_vo_omx_core.so:/system/lib/libomx_vo_omx_core.so \\
     vendor/htc/__DEVICE__/proprietary/libomx_wmadec_sharedlibrary.so:/system/lib/libomx_wmadec_sharedlibrary.so \\
     vendor/htc/__DEVICE__/proprietary/libomx_wmvdec_sharedlibrary.so:/system/lib/libomx_wmvdec_sharedlibrary.so \\
-    vendor/htc/__DEVICE__/proprietary/libon2.so:/system/lib/libon2.so \\
     vendor/htc/__DEVICE__/proprietary/libpvasfcommon.so:/system/lib/libpvasfcommon.so \\
     vendor/htc/__DEVICE__/proprietary/libpvasflocalpb.so:/system/lib/libpvasflocalpb.so \\
     vendor/htc/__DEVICE__/proprietary/libpvasflocalpbreg.so:/system/lib/libpvasflocalpbreg.so \\
-    vendor/htc/__DEVICE__/proprietary/libspeech.so:/system/lib/libspeech.so \\
-    vendor/htc/__DEVICE__/proprietary/voEVRCDec.so:/system/lib/voEVRCDec.so \\
-    vendor/htc/__DEVICE__/proprietary/voEVRCEnc.so:/system/lib/voEVRCEnc.so \\
-    vendor/htc/__DEVICE__/proprietary/voOMXOne.so:/system/lib/voOMXOne.so \\
-    vendor/htc/__DEVICE__/proprietary/voQCELPDec.so:/system/lib/voQCELPDec.so \\
-    vendor/htc/__DEVICE__/proprietary/voQCELPEnc.so:/system/lib/voQCELPEnc.so \\
-    vendor/htc/__DEVICE__/proprietary/voVidDec.so:/system/lib/voVidDec.so 
-
-
+    vendor/htc/__DEVICE__/proprietary/libspeech.so:/system/lib/libspeech.so
 
 EOF
 
