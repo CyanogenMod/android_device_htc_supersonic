99a6f4f5d0c256d1d0aa0011499883a0fcd1c84d
steve <shade@chemlab.org>
2010-09-22 18:02:42 -0400

    supersonic: Fix generic product name

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo generic_inc-userdebug
+add_lunch_combo generic_supersonic-userdebug
