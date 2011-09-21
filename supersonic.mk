635973c367927bcef7fdbc38436cfe77ca47d9a5
Keyan Mobli <persiansown@gmail.com>
2011-09-21 00:55:13 -0500

    Add front camera permission to Evo.
    Change-Id: Ieb28d23e883d2704d723f93945452e68b8d169a4

--- supersonic.mk
+++ supersonic.mk
@@ -48,6 +48,7 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/supersonic/overlay
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
